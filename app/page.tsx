f57c317e3e174d97573b9295e51bb3db88316add
jimbits <jimbits@jimhortons.com>
2025-11-01 12:13:46 -0600

    added routes

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import ContactForm from "@/components/forms/ContactFrom";
 export default function Home() {
   return (
     <section>
+      <header className="text-center my-10">
+        <h1 className="text-5xl   font-bold">Example Resend</h1>
+        <p>
+          Amet consectetur adipisicing elit. Placeat dolorem error corporis!
+        </p>
+      </header>
       <ContactForm />
     </section>
   );
